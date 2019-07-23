8bba50a296c5a4d1f9493eaed4d574df734dbbd7
Jamie Barton <jbarton10@email.arizona.edu>
2019-07-22 18:50:31 -0700

    Fixed ending connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,69 +2,80 @@ var inquirer = require('inquirer');
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "tobi1208",
     database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
+});
+
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
- 
-  });
 
+});
+
+//Call function that will display database to user
 displayItems();
 
 
 
 //Function for displaying everything in the database
-function displayItems(){
-    connection.query("SELECT * FROM products", function(err, res){
+function displayItems() {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err
         console.log("-----------------------------------");
         console.log("|  ID  |  Product Name  |  Price  |");
-        for (var i = 0; i < res.length; i++){
-            console.log("|  "+ res[i].item_id + "  |  "+ res[i].product_name + "  |  "+ res[i].price+ "  |");
+        for (var i = 0; i < res.length; i++) {
+            console.log("|  " + res[i].item_id + "  |  " + res[i].product_name + "  |  " + res[i].price + "  |");
         }
         console.log("-----------------------------------");
-        
-        
+
+
     });
+    //Funciton that will ask questions about 
     whatToBuy();
 }
 
-function whatToBuy(){
+function whatToBuy() {
     inquirer
-    .prompt([
-        {
-            message:"What would you like to buy?",
-            name: "itemToBuy",
-            type: "input"
-        },
-        {
-            message:"How many of these would you like to buy?",
-            name: "quantity",
-            type: "input"
-        }
+        .prompt([
+            {
+                message: "What would you like to buy?",
+                name: "itemToBuy",
+                type: "input"
+            },
+            {
+                message: "How many of these would you like to buy?",
+                name: "quantity",
+                type: "input"
+            }
 
-    ]).then(function(response){
-        console.log(response.itemToBuy)
-        connection.query("UPDATE products SET stock_quantity = stock_quantity - "+response.quantity+" WHERE stock_quantity > 0 AND product_name = '"+response.itemToBuy+"';", function(err,res){
-            if (err) throw err
+        ]).then(function (response) {
+            
+            //BETTER WAY TO MAKE SQL QUERIES
+            var selectQuery = "SELECT * FROM products WHERE product_name = ?"
+           connection.query(selectQuery, [response.itemToBuy], function (err, res) {
+                if (err) throw err
+                
+                if (res[0].stock_quantity >= 0 && (res[0].stock_quantity - response.quantity) >= 0) {
+                    connection.query("UPDATE products SET stock_quantity = stock_quantity - " + response.quantity + " WHERE stock_quantity > 0 AND product_name = '" + response.itemToBuy + "';", function (err, res) {
+                        if (err) throw err
 
-            console.log("Your order for "+ response.quantity + " " + response.itemToBuy + "(s) is complete!");
+                        console.log("Your order for " + response.quantity + " " + response.itemToBuy + "(s) is complete!");
+                        connection.end()
+                    });
+                }
+                else{
+                    console.log("Not enough stock!");
+                }
+            });
             connection.end()
+
         });
-
-        
-
-
-    });
 }
